e6795d7ba8a7a40dce8a7d72b68d9f2dc8cfbb19
Abhijit Paul <37068967+abhpaul@users.noreply.github.com>
2018-09-12 19:13:26 +0530

    Update tests.sh

--- result/tests/tests.sh
+++ result/tests/tests.sh
@@ -9,7 +9,7 @@ sleep 10
 
 if phantomjs render.js http://result | grep -q '1 vote'; then
   echo -e "\\e[42m------------"
-  echo -e "\\e[92mTests passed"
+  echo -e "\\e[92mTests passed test"
   echo -e "\\e[42m------------"
   exit 0
 else
